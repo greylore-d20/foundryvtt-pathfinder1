b473b534b5ff9f55cd69cdb94980b1b03b04d617
Tijmen Bok <mikauschekzen@gmail.com>
2020-04-05 02:35:48 +0200

    Fix some things about token configs

--- module/token-config.js
+++ module/token-config.js
@@ -4,7 +4,7 @@ export class TokenConfigPF extends TokenConfig {
   static get defaultOptions() {
     const options = super.defaultOptions;
     options.template = "systems/pf1/templates/scene/token-config.html";
-    options.height = 420;
+    options.height = 460;
     return options;
   }
 

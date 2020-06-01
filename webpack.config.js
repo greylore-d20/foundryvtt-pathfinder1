8cb6ee985dffb164797dae02140fc27361263083
Tijmen Bok <mikauschekzen@gmail.com>
2020-06-01 09:45:10 +0200

    Add webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
+const conf = {
   name: "pf1",
   entry: {
     index: "./pf1.js",
   },
   mode: "development",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+        terserOptions: {
+          compress: {
+            keep_classnames: true,
+          },
+        },
+      }),
+    ],
+  },
   output: {
     publicPath: "systems/pf1/dist/",
     filename: "pf1.js",
@@ -13,3 +29,5 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
 };
+
+module.exports = conf;

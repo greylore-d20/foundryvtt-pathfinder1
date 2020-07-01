f36d34821880473f5f7d45fe5c0ee3a204511596
Marco <marcodantonio49@gmail.com>
2020-07-01 09:41:19 +0000

    Update entity.js

--- module/actor/entity.js
+++ module/actor/entity.js
@@ -2789,7 +2789,7 @@ export class ActorPF extends Actor {
       for (let note of result) {
         note.notes = note.notes.filter(o => {
           return o.target === "misc" && o.subTarget === miscKey;
-        }).map(o => { return o.note; });
+        }).map(o => { return o.text; });
       }
 
       if (miscKey === "cmb" && this.data.data.attributes.cmbNotes != null && this.data.data.attributes.cmbNotes !== "") {

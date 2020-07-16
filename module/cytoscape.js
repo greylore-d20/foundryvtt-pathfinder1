efd7a5e102ad8344e4ecb38bd9904b48da4c1a9b
Tijmen Bok <mikauschekzen@gmail.com>
2020-07-16 02:08:20 +0200

    Remove cytoscape script

--- module/cytoscape.js
+++
@@ -1,8 +0,0 @@
-const cytoscape = require("cytoscape");
-const cxtmenu = require("cytoscape-cxtmenu");
-const edgehandles = require("cytoscape-edgehandles");
-
-cytoscape.use(cxtmenu);
-cytoscape.use(edgehandles);
-
-module.exports = cytoscape;

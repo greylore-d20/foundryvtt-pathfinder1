greylore-d20/foundryvtt-pathfinder1

commit c0195569d4e9cecc640bb2ce1730dc55444ab788

Improve actor sheet rendering performance

module/lib.js
@@ -34,7 +34,7 @@ export const alterRoll = function(str, add, multiply) {
 /**
 * Creates tabs for a sheet object
 */
-export const createTabs = function(html, tabGroups) {
+export const createTabs = function(html, tabGroups, existingTabs=null) {
   // Create recursive activation/callback function
   const _recursiveActivate = function(rtabs, tabName=null) {
     if (tabName == null) this._initialTab[rtabs.group] = rtabs.active;
@@ -43,76 +43,70 @@ export const createTabs = function(html, tabGroups) {
       this._initialTab[rtabs.group] = tabName;
     }
     
-    // Scroll to previous position
-    let scrollElems = html.find(`.scroll-${rtabs.group}`);
-    if (scrollElems.length === 0) scrollElems = html.find(`.tab[data-group="${rtabs.group}"]`);
-    for (let o of scrollElems) {
-      const scrollIndex = o.dataset.scrollIndex || rtabs.group;
-      o.scrollTop = this._scrollTab[scrollIndex];
-    }
-    
     // Recursively activate tabs
     for (let subTab of rtabs.subTabs) {
       _recursiveActivate.call(this, subTab, subTab.active);
     }
   };
+
+  // Recursively bind tabs
+  const _recursiveBind = function(rtabs) {
+    rtabs.bind(html[0]);
+    for (let subTab of rtabs.subTabs) {
+      _recursiveBind.call(this, subTab);
+    }
+  };
   
   // Create all tabs
-  const _func = function(group, children) {
+  const _func = function(group, children, tabs=null) {
     if (html.find(`nav[data-group="${group}"]`).length === 0) return null;
     
     if (this._initialTab == null) this._initialTab = {};
-    if (this._scrollTab == null) this._scrollTab = {};
     
     const subHtml = html.find(`.${group}-body > div[data-group="${group}"]`);
     const activeSubHtml = subHtml.filter(".active");
     const initial = this._initialTab[group] !== undefined ? this._initialTab[group] : (activeSubHtml.length > 0 ? activeSubHtml[0].dataset.tab : "");
     
     // Set up data for scroll position and active tab
-    if (this._scrollTab[group] === undefined) this._scrollTab[group] = 0;
     if (this._initialTab[group] === undefined) this._initialTab[group] = initial;
-    
-    // Set up scrolling callback
-    let scrollElems = html.find(`.scroll-${group}`);
-    if (scrollElems.length === 0) scrollElems = html.find(`.tab[data-group="${group}"]`);
-    scrollElems.scroll(ev => {
-      const scrollIndex = ev.currentTarget.dataset.scrollIndex || group;
-      this._scrollTab[scrollIndex] = ev.currentTarget.scrollTop;
-    });
     
     // Determine tab type
     const tabsElem = html.find(`.tabs[data-group="${group}"]`)[0];
     if (!tabsElem) return;
     let type = tabsElem.dataset.tabsType;
-    let cls = isMinimumCoreVersion("0.7.0") ? Tabs : TabsV2;
+    let cls = TabsV2;
     if (type === "list") {
       cls = ListTabs;
     }
     
     // Create tabs object
-    const tabs = new cls({
-      navSelector: `.tabs[data-group="${group}"]`,
-      contentSelector: `.${group}-body`,
-      callback: (_, tabs) => {
-        _recursiveActivate.call(this, tabs);
-      },
-    });
-    
-    // Recursively create tabs
-    tabs.group = group;
-    tabs.subTabs = [];
-    for (let [childKey, subChildren] of Object.entries(children)) {
-      const newTabs = _func.call(this, childKey, subChildren);
-      if (newTabs != null) tabs.subTabs.push(newTabs);
+    if (!tabs) {
+      tabs = new cls({
+        navSelector: `.tabs[data-group="${group}"]`,
+        contentSelector: `.${group}-body`,
+        callback: (_, tabs) => {
+          _recursiveActivate.call(this, tabs);
+        },
+      });
+      
+      // Recursively create tabs
+      tabs.group = group;
+      tabs.subTabs = [];
+      for (let [childKey, subChildren] of Object.entries(children)) {
+        const childTabs = _func.call(this, childKey, subChildren);
+        if (childTabs != null) {
+          tabs.subTabs.push(childTabs);
+          childTabs.parent = tabs;
+        }
+      }
     }
     
-    tabs.bind(html[0]);
-    _recursiveActivate.call(this, tabs, this._initialTab[group]);
+    _recursiveBind.call(this, tabs);
     return tabs;
   };
   
   for (const groupKey of Object.keys(tabGroups)) {
-    _func.call(this, groupKey, tabGroups[groupKey]);
+    return _func.call(this, groupKey, tabGroups[groupKey], existingTabs);
   }
 };
 
@@ -525,4 +519,33 @@ export const createConsumableSpell = async function(itemData, type) {
 
   if (data._id) delete data._id;
   return data;
+};
+
+export const adjustNumberByStringCommand = function(initialValue, cmdStr, maxValue=null) {
+
+  let result = initialValue;
+
+  if (cmdStr.match(/(\+|[=-]?-)([0-9]+)/)) {
+    const operator = RegExp.$1;
+    let value = parseInt(RegExp.$2);
+    if (operator === "--" || operator === "=-") {
+      result = -value;
+    }
+    else {
+      if (operator === "-") value = -value;
+      result = initialValue + value;
+      if (maxValue) result = Math.min(result, maxValue);
+    }
+  }
+  else if (cmdStr.match(/^[0-9]+$/)) {
+    result = parseInt(cmdStr);
+    if (maxValue) result = Math.min(result, maxValue);
+  }
+  else if (cmdStr === "") {
+    result = 0;
+  }
+  else result = parseFloat(cmdStr);
+
+  if (Number.isNaN(result)) result = initialValue;
+  return result;
 };

less/actors.less
@@ -177,7 +177,7 @@
           }
 
           div.experience {
-            input[type="text"], .max {
+            input[type="text"], span.text-box, .max {
               padding: 0;
               flex: 1;
               text-align: center;
@@ -307,7 +307,7 @@
   }
 
   // Sections
-  section {
+  .primary-body section {
     overflow-y: hidden;
   }
 
@@ -346,89 +346,10 @@
       margin: 0;
     }
 
-    li.natural-armor, li.generic-defenses {
-      flex-wrap: nowrap;
-      white-space: nowrap;
-      flex: 0.5;
-
-      h3 {
-        flex: 0;
-        margin: 0 8px;
-      }
-
-      input[type="text"] {
-        flex: 0 0 32px;
-      }
-    }
-
-    li.attribute.health {
-      height: 100%;
-      border-bottom: none;
-    }
-
-    li.attribute {
-      height: 80px;
-      margin: 0 5px;
-      border: @borderGroove;
-      border-radius: 4px;
-      .nodesto();
-      text-align: center;
-
-      .attribute-name {
-        flex: 0 0 18px;
-      }
-
-      .attribute-speed {
-        .attribute {
-          white-space: nowrap;
-          overflow: hidden;
-          text-align: center;
-        }
-      }
-
-      &.health {
-        div.attribute {
-          max-width: 33%;
-        }
-      }
-
-      .attribute-value {
-        height: 28px;
-        line-height: 20px;
-
-        input {
-          display: inline;
-          max-width: 80%;
-          margin: 0;
-          font-size: 24px;
-        }
-
-        div.attribute {
-          display: inline-block;
-          min-width: 24px;
-          font-size: 24px;
-          width: calc(100% - 2px);
-        }
-
-        span.sep {
-          display: inline;
-          position: relative;
-          top: 2px;
-          font-size: 28px;
-          color: @colorTan;
-        }
-
-        &.multiple {
-          input { max-width: 33%; }
-        }
-      }
-
-      .attribute-footer {
-        flex: 0 0 18px;
-        margin-top: -1px;
-        line-height: 18px;
-        font-family: "Signika", "Palatino Linotype", serif;
-        font-size: 14px;
+    table {
+      span.text-box, input[type="text"] {
+        height: 20px;
+        padding: 1px 3px;
       }
     }
   }
@@ -588,12 +509,6 @@
           top: -4px;
         }
 
-        input[type="text"] {
-          height: 24px;
-          width: initial;
-          padding: 0;
-        }
-
         .skill-control {
           display: flex;
           align-items: center;
@@ -618,10 +533,12 @@
 
       .skill-mod {
         flex: 0 0 28px;
+        line-height: 22px;
         font-weight: bold;
         margin: -1px 22px 0px 0px;
         text-align: center;
         border: 1px dotted #111;
+        cursor: initial;
       }
 
       .skill-rank {
@@ -632,6 +549,10 @@
 
       .skill-controls {
         flex: 0 0 24px;
+      }
+
+      input[type="text"] {
+        height: auto;
       }
     }
 
@@ -860,7 +781,8 @@
   .tab.inventory,
   .tab.spellbook,
   .tab.attributes,
-  .tab.combat {
+  .tab.combat,
+  .tab.buffs {
     overflow-y: hidden;
 
     label.checkbox {
@@ -873,10 +795,23 @@
       }
     }
 
+    .buffs-conditions {
+      flex: 0;
+    }
+
     .combat-attacks {
       flex: 1 1 320px;
       border-bottom: 2px solid @colorDark;
       margin-bottom: 4px;
+      overflow-y: scroll;
+    }
+
+    .inventory-group {
+      height: 100%;
+    }
+    
+    .inventory-list {
+      height: 100%;
       overflow-y: scroll;
     }
   }
@@ -950,6 +885,8 @@
     }
 
     .spell-points-current {
+      flex: 0;
+
       h3 {
         .nodesto();
         font-size: 22px;
@@ -967,7 +904,6 @@
       }
     }
 
-    .spell-slots,
     .spell-comps,
     .spell-uses {
       white-space: nowrap;

less/apps.less
@@ -369,7 +369,6 @@
       padding: 0;
       background: rgba(0, 0, 0, 0.05);
       border: @borderGroove;
-      font-weight: bold;
       line-height: 24px;
       flex: 0 0 24px;
 
@@ -773,11 +772,15 @@
       line-height: 20px;
       color: @colorTan;
     }
-    input[type="text"] {
+    input[type="text"], span.text-box {
       flex: 1;
       text-align: center;
       margin-left: 8px;
       border-bottom: @borderGroove;
+      padding: 1px 3px;
+      margin: 0;
+      height: auto;
+      line-height: 20px;
     }
     .convert-currency {
       flex: 0;

less/misc.less
@@ -72,6 +72,31 @@ form {
   color: white;
 }
 
+span.text-box {
+  display: inline-block;
+  text-align: center;
+  border: 1px solid rgba(0, 0, 0, 0);
+  border-radius: 3px;
+  color: black;
+  min-width: 20px;
+  width: calc(100% - 2px);
+  padding: 1px 3px;
+  cursor: text;
+  min-height: 20px;
+
+  &.readonly {
+    cursor: initial;
+  }
+  &:hover {
+    box-shadow: 0 0 8px red;
+    border-color: black;
+  }
+}
+
+input.text-box {
+  text-align: center;
+}
+
 .pf1 {
   table {
     margin: 4px 0;

module/actor/entity.js
@@ -1738,7 +1738,8 @@ export class ActorPF extends Actor {
 
   getRollData(data=null) {
     if (data == null) data = this.data.data;
-    let result = duplicate(data);
+    let result = data;
+    result.item = undefined;
 
     // Set size index
     result.size = Object.keys(CONFIG.PF1.sizeChart).indexOf(getProperty(data, "traits.size")) - 4;

module/actor/sheets/npc.js
@@ -77,9 +77,32 @@ export class ActorSheetPFNPC extends ActorSheetPF {
 
     // Rollable Health Formula
     html.find(".health .rollable").click(this._onRollHealthFormula.bind(this));
+
+    // Adjust CR
+    html.find("span.text-box.cr-input")
+    .on("click", event => { this._onSpanTextInput(event, this._adjustCR.bind(this)); });
   }
 
   /* -------------------------------------------- */
+
+  _adjustCR(event) {
+    event.preventDefault();
+    const el = event.currentTarget;
+
+    const value = CR.fromString(el.tagName.toUpperCase() === "INPUT" ? el.value : el.innerText);
+    const name = el.getAttribute("name");
+    if (name) {
+      this._pendingUpdates[name] = value;
+    }
+
+    // Update on lose focus
+    if (event.originalEvent instanceof MouseEvent) {
+      if (!this._submitQueued) {
+        $(el).one("mouseleave", event => { this._onSubmit(event); });
+      }
+    }
+    else this._onSubmit(event);
+  }
 
   /**
    * Handle rolling NPC health values using the provided formula

module/item/entity.js
@@ -2168,7 +2168,7 @@ export class ItemPF extends Item {
    */
   getRollData() {
     const result = this.parentActor != null ? this.parentActor.getRollData() : {};
-    result.item = duplicate(this.data.data);
+    result.item = this.data.data;
 
     if (this.type === "spell" && this.parentActor != null) {
       const spellbook = this.spellbook;

commit 9bef2d920a889c530eb8bf58e5274acdd2e4416a

Fixed spellbook not showing on new actors
This was broken since c0195569
Worked after a refresh, though

module/lib.js
@@ -37,6 +37,8 @@ export const alterRoll = function(str, add, multiply) {
 export const createTabs = function(html, tabGroups, existingTabs=null) {
   // Create recursive activation/callback function
   const _recursiveActivate = function(rtabs, tabName=null) {
+    if (rtabs.__dormant) return;
+
     if (tabName == null) this._initialTab[rtabs.group] = rtabs.active;
     else {
       rtabs.activate(tabName);
@@ -52,6 +54,10 @@ export const createTabs = function(html, tabGroups, existingTabs=null) {
   // Recursively bind tabs
   const _recursiveBind = function(rtabs) {
     rtabs.bind(html[0]);
+    
+    if (html.find(`nav[data-group="${rtabs.group}"]`).length > 0) rtabs.__dormant = false;
+    else rtabs.__dormant = true;
+
     for (let subTab of rtabs.subTabs) {
       _recursiveBind.call(this, subTab);
     }
@@ -59,7 +65,8 @@ export const createTabs = function(html, tabGroups, existingTabs=null) {
   
   // Create all tabs
   const _func = function(group, children, tabs=null) {
-    if (html.find(`nav[data-group="${group}"]`).length === 0) return null;
+    let dormant = false;
+    if (html.find(`nav[data-group="${group}"]`).length === 0) dormant = true;
     
     if (this._initialTab == null) this._initialTab = {};
     
@@ -72,11 +79,12 @@ export const createTabs = function(html, tabGroups, existingTabs=null) {
     
     // Determine tab type
     const tabsElem = html.find(`.tabs[data-group="${group}"]`)[0];
-    if (!tabsElem) return;
-    let type = tabsElem.dataset.tabsType;
     let cls = TabsV2;
-    if (type === "list") {
-      cls = ListTabs;
+    if (tabsElem) {
+      let type = tabsElem.dataset.tabsType;
+      if (type === "list") {
+        cls = ListTabs;
+      }
     }
     
     // Create tabs object
@@ -88,6 +96,7 @@ export const createTabs = function(html, tabGroups, existingTabs=null) {
           _recursiveActivate.call(this, tabs);
         },
       });
+      tabs.__dormant = dormant;
       
       // Recursively create tabs
       tabs.group = group;
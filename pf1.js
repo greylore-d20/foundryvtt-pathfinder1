f425b3b845b7954821745cb2c53eee61ce574887
Tijmen Bok <mikauschekzen@gmail.com>
2021-02-18 01:24:05 +0100

    Apply migration for next release

--- pf1.js
+++ pf1.js
@@ -276,7 +276,7 @@ Hooks.once("ready", async function () {
   });
 
   // Migrate data
-  const NEEDS_MIGRATION_VERSION = "0.77.9";
+  const NEEDS_MIGRATION_VERSION = "0.77.10";
   let PREVIOUS_MIGRATION_VERSION = game.settings.get("pf1", "systemMigrationVersion");
   if (typeof PREVIOUS_MIGRATION_VERSION === "number") {
     PREVIOUS_MIGRATION_VERSION = PREVIOUS_MIGRATION_VERSION.toString() + ".0";

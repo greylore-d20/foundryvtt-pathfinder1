7540abcea9afa3608b83a5fabaa902fba432e277
Ethaks <ethaks@protonmail.ch>
2021-03-13 19:20:12 +0100

    Fix release manifest link generation

--- getChangelog.js
+++ getChangelog.js
@@ -7,7 +7,7 @@ try {
   const manifestFile = fs.readFileSync("system.json", "utf-8");
   const manifest = JSON.parse(manifestFile.toString());
   let url = manifest.manifest;
-  url = url.replaceAll("workflow", manifest.version);
+  url = url.replaceAll("latest", manifest.version);
 
   const releaseNotes = `**Manifest URL: ${url}**\n\n${recentChanges}`;
   fs.writeFileSync("recent-changes.md", releaseNotes);

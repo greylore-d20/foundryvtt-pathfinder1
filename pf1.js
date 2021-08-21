83245caebff98a1865589037ceeb718fb6cf7dc8
Tijmen Bok <mikauschekzen@gmail.com>
2021-08-21 07:30:38 +0200

    Migrate compendiums
    Update compatible core version

--- pf1.js
+++ pf1.js
@@ -446,7 +446,7 @@ Hooks.once("ready", async function () {
   });
 
   // Migrate data
-  const NEEDS_MIGRATION_VERSION = "0.78.14";
+  const NEEDS_MIGRATION_VERSION = "0.79.0";
   let PREVIOUS_MIGRATION_VERSION = game.settings.get("pf1", "systemMigrationVersion");
   if (typeof PREVIOUS_MIGRATION_VERSION === "number") {
     PREVIOUS_MIGRATION_VERSION = PREVIOUS_MIGRATION_VERSION.toString() + ".0";

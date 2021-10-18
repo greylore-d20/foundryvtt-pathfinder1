63616fb19823fe94a716e93cb2fd2a7878575b20
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2021-10-18 19:59:19 +0300

    Label entangled attack penalty

--- module/actor/apply-changes.js
+++ module/actor/apply-changes.js
@@ -1254,6 +1254,7 @@ export const addDefaultChanges = function (changes) {
             target: "attack",
             subTarget: "attack",
             modifier: "penalty",
+            flavor: game.i18n.localize("PF1.CondEntangled"),
           })
         );
         getSourceInfo(this.sourceInfo, "data.attributes.attack.general").negative.push({

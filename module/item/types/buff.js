cc26bae05910e64a06e212f40f015dd784e545f1
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2022-01-10 17:57:48 +0200

    Restore buff preparation

--- module/item/types/buff.js
+++ module/item/types/buff.js
@@ -1,6 +1,39 @@
 import { ItemPF } from "../entity.js";
 
 export class ItemBuffPF extends ItemPF {
+  prepareDerivedItemData() {
+    super.prepareDerivedItemData();
+    const itemData = this.data.data;
+
+    // Add total duration in seconds
+    if (itemData.duration.value?.length) {
+      let seconds = 0;
+      const rollData = this.getRollData();
+      const duration = RollPF.safeRoll(itemData.duration.value || "0", rollData).total;
+      switch (itemData.duration.units) {
+        case "hour":
+          seconds = duration * 60 * 60;
+          break;
+        case "minute":
+          seconds = duration * 60;
+          break;
+        case "rounds":
+          seconds = duration * 6;
+          break;
+      }
+
+      itemData.duration.totalSeconds = seconds;
+    }
+  }
+
+  getRollData() {
+    const result = super.getRollData();
+
+    result.item.level = this.data.data.level;
+
+    return result;
+  }
+
   get isActive() {
     return this.data.data.active;
   }

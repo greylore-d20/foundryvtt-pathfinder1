aa44dd95400058611e92445ca8025d6f9f88d8a9
Tijmen Bok <mikauschekzen@gmail.com>
2022-03-18 14:21:16 +0100

    Generate change ID if the change doesn't have one
    Fixes #1340

--- module/item/entity.js
+++ module/item/entity.js
@@ -48,7 +48,7 @@ export class ItemPF extends ItemBasePF {
       for (const c of changes) {
         let i = 0;
         // Forcibly seek unused ID.
-        while (actor.changes.get(c._id) !== undefined) {
+        while (actor.changes.get(c._id) !== undefined || !c._id) {
           updated = true;
           // Revert to default ID generation if too many iterations have passed. Just let it break if even more has passed.
           if (i > 10_000) break;

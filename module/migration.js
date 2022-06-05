8221bdea8d23af48c33c52b8b467f6c8bb3f00cc
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2022-06-05 16:02:45 +0300

    No currency migration if nothing is wrong

--- module/migration.js
+++ module/migration.js
@@ -1234,7 +1234,11 @@ const _migrateBuggedValues = function (ent, updateData, linked) {
     "data.altCurrency.cp",
   ];
   for (const key of convertToInt) {
-    updateData[key] = parseInt(getProperty(ent, key)) || 0;
+    const oldValue = getProperty(ent, key),
+      value = parseInt(oldValue ?? 0);
+    if (oldValue != value) {
+      updateData[key] = value;
+    }
   }
 };
 

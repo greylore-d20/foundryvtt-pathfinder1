c9de1311358d2e2671be39658efb4a11f01f37ae
Ethaks <ethaks@protonmail.ch>
2022-08-10 02:46:20 +0200

    Remove WIP base component from live code

--- module/components/_module.mjs
+++ module/components/_module.mjs
@@ -1,6 +1,4 @@
 export { ItemAction } from "./action.mjs";
 export { ItemChange } from "./change.mjs";
-//export * as common from "./component-base.mjs";
-export { DocumentComponentPF } from "./component-base.mjs";
 export { ItemConditional, ItemConditionalModifier } from "./conditionals.mjs";
 export { ItemScriptCall } from "./script-call.mjs";

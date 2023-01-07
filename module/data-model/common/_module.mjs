08d85194181338e3a9e87fac7a564039d95a12b5
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2023-01-07 17:23:24 +0200

    Common DataModel module

---
+++ module/data-model/common/_module.mjs
@@ -0,0 +1,5 @@
+export { ChangeModel } from "./change-model.mjs";
+export { ContextNoteModel } from "./context-note-mode.mjs";
+export { ChangeFlagsModel } from "./change-flags-model.mjs";
+export { TraitModel } from "./trait-model.mjs";
+export { ScriptCallModel } from "./script-call-model.mjs";

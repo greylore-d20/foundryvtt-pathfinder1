80b6a3a204a6bda6bbd5451aab96480d4ddc1614
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2023-01-17 20:16:17 +0200

    Add Class DataModel

--- module/data-model/item/_module.mjs
+++ module/data-model/item/_module.mjs
@@ -1,3 +1,4 @@
 export { BuffItemModel } from "./buff-model.mjs";
+export { ClassItemModel } from "./class-model.mjs";
 export { LootItemModel } from "./loot-model.mjs";
 export { RaceItemModel } from "./race-model.mjs";

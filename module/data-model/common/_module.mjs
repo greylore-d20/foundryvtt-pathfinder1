ec9147a8a1be704f904440840d761cd790f01910
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2023-01-17 15:49:12 +0200

    Export LinkDataModel

--- module/data-model/common/_module.mjs
+++ module/data-model/common/_module.mjs
@@ -2,4 +2,5 @@ export { ChangeModel } from "./change-model.mjs";
 export { ContextNoteModel } from "./context-note-mode.mjs";
 export { ChangeFlagsModel } from "./change-flags-model.mjs";
 export { TraitModel } from "./trait-model.mjs";
+export { LinkDataModel } from "./link-data-model.mjs";
 export { ScriptCallModel } from "./script-call-model.mjs";

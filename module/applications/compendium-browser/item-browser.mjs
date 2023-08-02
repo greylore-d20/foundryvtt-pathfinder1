fc411ff9fd231678c1a808e50cc86c8a34652401
Ethaks <ethaks@protonmail.ch>
2023-08-02 14:32:18 +0200

    fix: remove invalid equipmentSubtype for item browser entries

--- module/applications/compendium-browser/item-browser.mjs
+++ module/applications/compendium-browser/item-browser.mjs
@@ -20,4 +20,24 @@ export class ItemBrowser extends CompendiumBrowser {
     itemFilters.ItemCasterLevelFilter,
     commonFilters.TagFilter,
   ];
+
+  /** @override */
+  static _mapEntry(entry, pack) {
+    const result = super._mapEntry(entry, pack);
+
+    // Remove equipmentSubtype if the item subtype should not have one
+    const { subType, equipmentSubtype } = result.system;
+    if (subType) {
+      const equipmentSubtypes = Object.keys(pf1.config.equipmentTypes[subType] ?? {}).filter((o) => !o.startsWith("_"));
+      if (equipmentSubtypes.length === 0) {
+        // Clear equipmentSubtype if subType has no equipmentSubtypes
+        result.system.equipmentSubtype = "";
+      } else if (!equipmentSubtypes.includes(equipmentSubtype)) {
+        // Default to first equipmentSubtype if current equipmentSubtype is invalid
+        result.system.equipmentSubtype = equipmentSubtypes.at(0);
+      }
+    }
+
+    return result;
+  }
 }

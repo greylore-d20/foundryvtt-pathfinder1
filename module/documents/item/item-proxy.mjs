78d3222463ade70c2a8733c406c6f13cfbfefe53
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2023-08-05 23:06:24 +0300

    fix: nondescript errors in actor&item proxy constructor
    Fixes #2411

--- module/documents/item/item-proxy.mjs
+++ module/documents/item/item-proxy.mjs
@@ -2,7 +2,10 @@ import { ItemPF } from "./item-pf.mjs";
 
 const itemHandler = {
   construct(_, args) {
-    return new CONFIG.Item.documentClasses[args[0]?.type](...args);
+    const type = args[0]?.type;
+    const cls = CONFIG.Item.documentClasses[type];
+    if (!cls) throw new Error(`"${type}" is not valid item type`);
+    return new cls(...args);
   },
 };
 

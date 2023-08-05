7a42814d35879447d5fdb5c79ff33725a38901e3
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2023-08-05 23:06:24 +0300

    fix: nondescript errors in actor&item proxy constructor
    Fixes #2411

--- module/documents/actor/actor-proxy.mjs
+++ module/documents/actor/actor-proxy.mjs
@@ -2,7 +2,10 @@ import { ActorPF } from "./actor-pf.mjs";
 
 const actorHandler = {
   construct(_, args) {
-    return new CONFIG.Actor.documentClasses[args[0]?.type](...args);
+    const type = args[0]?.type;
+    const cls = CONFIG.Actor.documentClasses[type];
+    if (!cls) throw new Error(`"${type}" is not valid actor type`);
+    return new cls(...args);
   },
 };
 

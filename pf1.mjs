3bed2283c3850da3de307c0316c8b0d7fe668142
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2023-08-16 01:59:06 +0300

    fix: late pre-translation
    Fixes #2443

--- pf1.mjs
+++ pf1.mjs
@@ -258,10 +258,8 @@ if (import.meta.env.DEV) {
 /*  Foundry VTT Setup                           */
 /* -------------------------------------------- */
 
-/**
- * This function runs after game data has been requested and loaded from the servers, so documents exist
- */
-Hooks.once("setup", function () {
+// Pre-translation passes
+Hooks.once("i18nInit", function () {
   // Localize CONFIG objects once up-front
   const toLocalize = [
     "abilities",
@@ -383,7 +381,12 @@ Hooks.once("setup", function () {
       pf1.config[l][k].label = game.i18n.localize(v.label);
     }
   }
+});
 
+/**
+ * This function runs after game data has been requested and loaded from the servers, so documents exist
+ */
+Hooks.once("setup", () => {
   // Prepare registry data
   for (const registry of Object.values(pf1.registry)) {
     if (registry instanceof pf1.registry.Registry) registry.setup();

fc46750b3b391829898dfb3e3b75918065841698
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2023-09-11 04:47:06 +0300

    fix path

--- module/tours.mjs
+++ module/tours.mjs
@@ -2,6 +2,6 @@ Hooks.once("ready", async function registerPF1Tours() {
   game.tours.register(
     "pf1",
     "foundryExtensions",
-    await SidebarTour.fromJSON("/systems/pf1/tours/foundry-extensions.json")
+    await SidebarTour.fromJSON("systems/pf1/tours/foundry-extensions.json")
   );
 });

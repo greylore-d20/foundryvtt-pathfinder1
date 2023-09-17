53b08f7a66afa11f7bb068c9515e332665ae9a11
M.A <3460242-mkahvi@users.noreply.gitlab.com>
2023-09-17 19:37:10 +0300

    chore: remove unused ItemAttackPF function

--- module/documents/item/item-attack.mjs
+++ module/documents/item/item-attack.mjs
@@ -1,11 +1,3 @@
 import { ItemPF } from "./item-pf.mjs";
 
-export class ItemAttackPF extends ItemPF {
-  getConditionalTargets() {
-    const result = super.getConditionalTargets();
-
-    result["size"] = game.i18n.localize(pf1.config.conditionalTargets.size._label);
-
-    return result;
-  }
-}
+export class ItemAttackPF extends ItemPF {}

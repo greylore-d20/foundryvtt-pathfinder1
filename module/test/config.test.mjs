bde3e56b93514a1812d289507b6835e415c025ce
M.A. <3460242-mkahvi@users.noreply.gitlab.com>
2024-05-08 00:52:30 +0300

    test: validate strings are translated

--- module/test/config.test.mjs
+++ module/test/config.test.mjs
@@ -5,7 +5,7 @@ export function registerConfigTests() {
       const { describe, it, expect, after, assert } = context;
 
       describe("i18n", function () {
-        it("Pre-translation", function () {
+        it("No untranslated strings", function () {
           const flatConfig = foundry.utils.flattenObject(pf1.config);
           // Log bad values first
           for (const [path, value] of Object.entries(flatConfig)) {
@@ -20,6 +20,52 @@ export function registerConfigTests() {
           Object.values(flatConfig)
             .filter((value) => typeof value === "string")
             .every((value) => expect(value).to.not.match(/^PF1\./, "Not valid or translated."));
+        });
+
+        it("Valid translations", function () {
+          // Ignore paths, these include mostly internal IDs.
+          const ignore = [
+            /buffTargets\..*category$/,
+            /contextNoteTargets\..*category$/,
+            /^classCasterType\./,
+            /favouredClassBonusIcons\./,
+            /levelAbilityScoreFeature\./,
+            /measureTemplateTypes\./, // Foundry doesn't provide translations for these
+            /sizeChart\./,
+          ];
+
+          const configi18n = Object.entries(foundry.utils.flattenObject(pf1.config))
+            .filter(([path, value]) => {
+              if (typeof value !== "string") return false;
+
+              if (value.length === 0) return false;
+
+              if (ignore.some((re) => re.test(path))) return false;
+
+              // Catch paths
+              if (/\/.*\.\w+$/.test(value)) return false;
+
+              try {
+                if (fromUuidSync(value)) return false;
+              } catch (e) {
+                return false; // Embedded document link
+              }
+
+              // Assume strings that can be parsed as formulas are not i18n strings
+              try {
+                const roll = Roll.defaultImplementation.create(value).evaluate({ async: false });
+                return false;
+              } catch (e) {
+                /* nop */
+              }
+              console.log(path, value);
+              return true;
+            })
+            .map(([_, value]) => value);
+
+          const sysi18n = new Set(Object.values(foundry.utils.flattenObject(game.i18n.translations)));
+
+          configi18n.every((value) => expect(sysi18n).to.contain(value));
         });
       });
     },

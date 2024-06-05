81779e8a5a85fabaf6b507d5c7eae572ae2c8abf
M.A. <3460242-mkahvi@users.noreply.gitlab.com>
2024-06-05 13:01:11 +0300

    build: smarter empty string culling
    If the default is non-empty string and we cull it, it can lead to template.json filling incorrect data for the item.

--- tools/packs.mjs
+++ tools/packs.mjs
@@ -427,8 +427,8 @@ function enforceTemplate(object, template, options = {}) {
     const currentValue = utils.getProperty(object, path);
     const templateValue = utils.getProperty(template, path);
     if (templateValue === "" && currentValue === null) delete flattened[path];
-    // Delete empty strings in general
-    if (currentValue === "") delete flattened[path];
+    // Delete empty strings in general if they don't default to something more specific
+    if (currentValue === "" && !(utils.getProperty(template, path)?.length > 0)) delete flattened[path];
 
     const templateHasArray = Array.isArray(utils.getProperty(template, path));
     const isEmptyArray = flattened[path] instanceof Array && flattened[path].length === 0;

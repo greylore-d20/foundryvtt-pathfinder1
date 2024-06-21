9c8f86a76756b6720093dcf65a71013be4b38fdf
Chechu-さん <suso.becerra98@gmail.com>
2024-06-21 10:09:16 +0100

    refactor: create util functions to reduce static access of step index

--- module/tours/base-tours.mjs
+++ module/tours/base-tours.mjs
@@ -4,6 +4,102 @@ export class PF1Tour extends Tour {
   calledAsReset = false;
 
   /**
+   * @returns {TourStep} The previous step.
+   */
+  get previousStep() {
+    return this.hasPrevious ? this.steps[this.stepIndex - 1] : undefined;
+  }
+
+  /**
+   * Gets the current sheet based on if it's displayed or not.
+   * WARNING: This won't work if the actor sheet is not displayed or there are multiple actor sheets open at once.
+   *
+   * @returns {import("@app/actor/actor-sheet.mjs").ActorSheetPF | null} The current actor sheet if found.
+   */
+  get sheetInDisplay() {
+    // We need to Actor ID to find the sheet. The actor ID is in the ID of the sheet element but needs some formatting
+    const sheetAppID = document.querySelector("div.sheet.pf1.actor")?.getAttribute("data-appid");
+    if (!sheetAppID) return null;
+    return ui.windows[sheetAppID];
+  }
+
+  /**
+   * In case you need to wait for the actor sheet to be displayed.
+   * WARNING: This blocks the code until the actor sheet is displayed.
+   *
+   * @returns {Promise<import("@app/actor/actor-sheet.mjs").ActorSheetPF | null>} The current actor sheet if found.
+   */
+  async waitForSheetInDisplay() {
+    const sheetSelector = "div.sheet.pf1.actor";
+    await this.waitForElement(sheetSelector);
+    return this.sheetInDisplay;
+  }
+
+  /**
+   * Given the ID of a step, returns the step from the steps array.
+   *
+   * @param {string} id The ID of the step.
+   * @returns {TourStep | undefined} The step with the given ID.
+   */
+  getStepById(id) {
+    return this.steps.find((step) => step.id === id);
+  }
+
+  /**
+   * Given the ID of a step, returns the step index associated with it.
+   *
+   * @param {string} id The ID of the step.
+   * @returns {number} The step index with the given ID (-1 if not found).
+   */
+  getStepIndexById(id) {
+    return this.steps.findIndex((step) => step.id === id);
+  }
+
+  /**
+   * In many steps we might need an specific compendium to be opened to continue.
+   *
+   * @param {string} compendium The compendium to open.
+   * @see {@link pf1.applications.compendiumBrowser.CompendiumBrowser.initializeBrowsers}
+   * @returns {Application} The opened compendium.
+   */
+  _openCompendium(compendium) {
+    if (!Object.prototype.hasOwnProperty.call(pf1.applications.compendiums, compendium)) {
+      throw new Error(`Compendium "${compendium}" not found`);
+    }
+    // Open compendium
+    return pf1.applications.compendiums[compendium].render(true, { focus: true });
+  }
+
+  /**
+   * This log functions outputs the given text to the console with Tour ID in the log message.
+   *
+   * @param {string} text The text to log.
+   * @param {"log"|"debug"|"warn"} loglevel The log level to use.
+   * @param {...any} args Extra arguments to pass to {@link console.info}.
+   */
+  _log(text, loglevel = "log", ...args) {
+    console[loglevel](`[Tour Step "%s.%s.%s"]: ${text}`, ...[this.namespace, this.id, this.currentStep?.id, ...args]);
+  }
+
+  /**
+   * @param {string} text The text to log.
+   * @param  {...any} args Extra arguments to pass to {@link console.info}.
+   * @see {@link _log}
+   */
+  _debug(text, ...args) {
+    this._log(text, "debug", ...args);
+  }
+
+  /**
+   * @param {string} text The text to log.
+   * @param  {...any} args Extra arguments to pass to {@link console.info}.
+   * @see {@link _log}
+   */
+  _warn(text, ...args) {
+    this._log(text, "warn", ...args);
+  }
+
+  /**
    * Foundry Tours do not wait for the element to be rendered so we need this workaround.
    *
    * @param {string} selector CSS selector of the element to wait for
@@ -46,6 +142,7 @@ export class PF1Tour extends Tour {
 
   /** @override */
   async _preStep() {
+    // We will always want `this.targetElement` to be there
     await this.waitForElement(this.currentStep?.selector);
     await super._preStep();
   }
@@ -84,42 +181,8 @@ export class PF1Tour extends Tour {
   }
 
   /** @override */
-  async exit() {
+  exit() {
     this.calledAsExit = true;
-    await super.exit();
-  }
-
-  /**
-   * @returns {TourStep} The previous step.
-   */
-  get previousStep() {
-    return this.hasPrevious ? this.steps[this.stepIndex - 1] : undefined;
-  }
-
-  /**
-   * Gets the current sheet based on if it's displayed or not.
-   * WARNING: This won't work if the actor sheet is not displayed or there are multiple actor sheets open at once.
-   *
-   * @returns {import("@app/actor/actor-sheet.mjs").ActorSheetPF | null} The current actor sheet if found.
-   */
-  get sheetInDisplay() {
-    // We need to Actor ID to find the sheet. The actor ID is in the ID of the sheet element but needs some formatting
-    const sheetAppID = document.querySelector("div.sheet.pf1.actor")?.getAttribute("data-appid");
-    if (!sheetAppID) return null;
-    return ui.windows[sheetAppID];
-  }
-
-  /**
-   * In case you need to wait for the actor sheet to be displayed.
-   * WARNING: This blocks the code until the actor sheet is displayed.
-   *
-   * @returns {Promise<import("@app/actor/actor-sheet.mjs").ActorSheetPF | null>} The current actor sheet if found.
-   */
-  async waitForSheetInDisplay() {
-    const sheetSelector = "div.sheet.pf1.actor";
-    await this.waitForElement(sheetSelector);
-    return this.sheetInDisplay;
+    super.exit();
   }
 }
-
-export class PF1SidebarTour extends PF1Tour {}
--- module/tours/create-npc.mjs
+++ module/tours/create-npc.mjs
@@ -1,6 +1,9 @@
-import { PF1SidebarTour } from "./base-tours.mjs";
+import { PF1Tour } from "./base-tours.mjs";
 
-export class CreateNPCTour extends PF1SidebarTour {
+export class CreateNPCTour extends PF1Tour {
+  /**
+   * Steps of the tour. Try to keep synced with `create-npc.json`.
+   */
   STEPS = {
     GO_TO_ACTORS_TAB: "goto-actors",
     SET_NAME: "set-name",
@@ -28,32 +31,44 @@ export class CreateNPCTour extends PF1SidebarTour {
 
   /**
    * In many steps we need this menu to be opened to continue.
-   * FIXME: Find a better way to do this since it relies on the selector of the first step
    */
   _openCreateActorMenu() {
     // Open Create Actor menu
-    document.querySelector(this.steps[0]?.selector)?.click();
+    /** @type {ActorDirectory} */
+    const actorsTab = ui.sidebar.tabs.actors;
+    actorsTab.activate();
+    actorsTab.element?.find("button.create-document")?.click();
   }
 
   /**
    * In many steps we need this sheet to be opened to continue.
+   *
+   * @returns {import("@app/actor/actor-sheet.mjs").ActorSheetPF | null} The opened Actor sheet.
    */
-  _openActorSheet() {
+  _openCreatedNPCSheet() {
+    if (!this.createdNPC) {
+      this._warn("Trying to open a Sheet for an Actor that doesn't exist in the Tour object.");
+      ui.notifications.warn("PF1.Tours.RestartTutorialWarning", { localize: true });
+      return null;
+    }
+    /** @type {import("@app/actor/actor-sheet.mjs").ActorSheetPF | null} */
+    const sheet = this.createdNPC.sheet;
+    if (!sheet) return null;
     // Open Actor sheet
-    this.createdNPC?.sheet?.render(true);
+    return sheet.render(true, { focus: true });
   }
 
   async _setActorName() {
     // Not necessary but funny. Some random NPC names so it's not always Bob or so
     const randomNamesForNPCs = ["Generic Smith", "Bob Rando", "Droopy McCool", "Smoki Bacon", "Meep", "Bathroom Bobby"];
     /** @type {HTMLInputElement} */
-    const nameInput = await this.waitForElement(this.steps[1]?.selector);
+    const nameInput = await this.waitForElement(this.getStepById(this.STEPS.SET_NAME)?.selector);
     nameInput.value = randomNamesForNPCs[Math.floor(Math.random() * randomNamesForNPCs.length)];
   }
 
   async _setActorTypeAsNPC() {
     /** @type {HTMLSelectElement} */
-    const actorTypeSelect = await this.waitForElement(this.steps[2]?.selector);
+    const actorTypeSelect = await this.waitForElement(this.getStepById(this.STEPS.SET_TYPE)?.selector);
     actorTypeSelect.value = "npc";
   }
 
@@ -68,7 +83,7 @@ export class CreateNPCTour extends PF1SidebarTour {
   /** @override */
   async start() {
     game.togglePause(true);
-    ui.sidebar.activateTab("actors");
+    ui.sidebar.tabs.actors.activate();
 
     // We want all dialogs to be closed for a fresh start
     Object.values(ui.windows)
@@ -76,52 +91,68 @@ export class CreateNPCTour extends PF1SidebarTour {
       .forEach((app) => app.close());
 
     // Depending on the step we start we need to have the Create Actor menu open
-    if (this.stepIndex >= 2 && this.stepIndex <= 3) {
+    if (
+      this.stepIndex >= this.getStepIndexById(this.STEPS.SET_NAME) &&
+      this.stepIndex <= this.getStepIndexById(this.STEPS.CLICK_CREATE)
+    ) {
       this._openCreateActorMenu();
       await this._prepareDefaultCreationMenuData();
     }
 
     // Depending on the step we start we need to have the Actor sheet open
-    if (this.stepIndex >= 4) {
-      this._openActorSheet();
+    if (this.stepIndex >= this.getStepIndexById(this.STEPS.HIGHLIGHT_SHEET)) {
+      this._openCreatedNPCSheet();
+    }
+
+    // Depending on the step we start we need to have the race compendium open
+    if (this.stepIndex >= this.getStepIndexById(this.STEPS.ALL_RACES_DISPLAY)) {
+      this._openCompendium("races");
     }
 
     await super.start();
   }
 
   /** @override */
+  async progress(stepIndex) {
+    // If the user is coming from pressing "Previous" we don't want to open the Dialog again
+    if (this.steps[stepIndex]?.id === this.STEPS.SET_NAME && !this.calledAsPreviousStep) {
+      this._openCreateActorMenu();
+    }
+
+    await super.progress(stepIndex);
+  }
+
+  /** @override */
   async _preStep() {
+    await super._preStep();
+
     /** @type {string | undefined} */
     const currentStepId = this.currentStep?.id;
 
     switch (currentStepId) {
       case this.STEPS.SET_NAME:
-        if (!this.calledAsPreviousStep) {
-          this._openCreateActorMenu();
-        }
         await this._setActorName();
         break;
       case this.STEPS.SET_TYPE:
         await this._setActorTypeAsNPC();
         break;
-      case this.STEPS.HIGHLIGHT_SHEET:
-        this.createdNPC = (await this.waitForSheetInDisplay())?.actor;
-        break;
       case this.STEPS.ALL_RACES_DISPLAY:
         // Clean up the search bar in case it's not empty
-        await this._typeInInputField(this.steps[8]?.selector, "");
+        await this._typeInInputField(this.getStepById(this.STEPS.FILTER_HIGHLIGHT)?.selector, "");
         break;
       case this.STEPS.HIGHLIGHT_RACE:
         // Write race name in the search bar
         // We need to trigger the event like we were writing the name in order for the browser to update the filter
         // NOTE: This is assuming step 8 is `FILTER_HIGHLIGHT`
-        await this._typeInInputField(this.steps[8]?.selector, "gnome");
+        await this._typeInInputField(this.getStepById(this.STEPS.FILTER_HIGHLIGHT)?.selector, "gnome");
         break;
       case this.STEPS.HIGHLIGHT_SELECTED_RACE:
         // Trigger drag and drop event into the sheet
         this.sheetInDisplay?._onDropItem(new DragEvent("drop"), {
           type: "Item",
-          uuid: document.querySelector(this.previousStep?.selector)?.getAttribute("data-uuid"),
+          uuid: document
+            .querySelector(this.getStepById(this.STEPS.HIGHLIGHT_RACE)?.selector)
+            ?.getAttribute("data-uuid"),
         });
         // Close the race CompendiumBrowser
         ui.activeWindow.close();
@@ -129,8 +160,6 @@ export class CreateNPCTour extends PF1SidebarTour {
       default:
         break;
     }
-
-    await super._preStep();
   }
 
   /** @override */
@@ -146,7 +175,9 @@ export class CreateNPCTour extends PF1SidebarTour {
           break;
         }
         // Create the NPC Actor otherwise
-        this.targetElement.click();
+        this.targetElement?.click();
+        // Set the variable `createdNPC` to the created NPC
+        this.createdNPC = (await this.waitForSheetInDisplay())?.actor;
         break;
       case this.STEPS.OPEN_RACE_COMPENDIUM:
         // If the user is exiting the tour in this step or comes from pressing "Previous"
@@ -188,31 +219,34 @@ export class CreateNPCTour extends PF1SidebarTour {
   }
 
   /** @override */
-  async exit() {
-    /** @type {string | undefined} */
-    const currentStepId = this.currentStep?.id;
-
-    switch (currentStepId) {
-      // Close the Create Actor dialog that should be active
-      case this.STEPS.SET_NAME:
-        ui.activeWindow.close();
-        break;
-      case this.STEPS.SET_TYPE:
-        ui.activeWindow.close();
-        break;
-      case this.STEPS.CLICK_CREATE:
-        ui.activeWindow.close();
-        break;
-      case this.STEPS.HIGHLIGHT_SHEET:
-        ui.activeWindow.close();
-        break;
-      case this.STEPS.OPEN_RACE_COMPENDIUM:
-        ui.activeWindow.close();
-        break;
-      default:
-        break;
+  exit() {
+    // Close Create Actor Dialog
+    if (
+      this.stepIndex >= this.getStepIndexById(this.STEPS.SET_NAME) &&
+      this.stepIndex <= this.getStepIndexById(this.STEPS.CLICK_CREATE)
+    ) {
+      this._debug("Closing Create Actor Dialog");
+      ui.activeWindow.close();
     }
 
-    await super.exit();
+    // Close NPC Sheet
+    if (
+      this.stepIndex >= this.getStepIndexById(this.STEPS.HIGHLIGHT_SHEET) &&
+      this.stepIndex <= this.getStepIndexById(this.STEPS.OPEN_RACE_COMPENDIUM)
+    ) {
+      this._debug("Closing NPC Sheet");
+      ui.activeWindow.close();
+    }
+
+    // Close race Compendium and NPC Sheet
+    if (this.stepIndex >= this.getStepIndexById(this.STEPS.ALL_RACES_DISPLAY)) {
+      this._debug("Closing race Compendium");
+      ui.activeWindow.close();
+
+      this._debug("Closing NPC Sheet");
+      this.sheetInDisplay?.close();
+    }
+
+    super.exit();
   }
 }

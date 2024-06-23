2e23b2b300e25b0c2ba230e763bff71902e26bd9
Ethaks <ethaks@protonmail.ch>
2024-06-23 16:09:37 +0200

    chore: increase migration version

--- pf1.mjs
+++ pf1.mjs
@@ -584,7 +584,7 @@ Hooks.once("ready", async function () {
   });
 
   // Migrate data
-  const NEEDS_MIGRATION_VERSION = "10.0";
+  const NEEDS_MIGRATION_VERSION = "10.2";
   let PREVIOUS_MIGRATION_VERSION = game.settings.get("pf1", "systemMigrationVersion");
   if (typeof PREVIOUS_MIGRATION_VERSION === "number") {
     PREVIOUS_MIGRATION_VERSION = PREVIOUS_MIGRATION_VERSION.toString() + ".0";

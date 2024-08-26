45f2efa0acb0f7f4189314787f3011f46757c1e8
M.A. <3460242-mkahvi@users.noreply.gitlab.com>
2024-08-26 23:56:46 +0300

    fix: CONFIG.statusEffects data format

--- module/utils/initialization.mjs
+++ module/utils/initialization.mjs
@@ -3,13 +3,18 @@
  */
 export const getConditions = function () {
   const core = CONFIG.statusEffects.filter((c) => c.id !== "dead");
-  let sys = pf1.registry.conditions.map((condition) => {
-    return {
-      id: condition.id,
-      label: condition.name,
-      icon: condition.texture,
-    };
-  });
+  let sys = pf1.registry.conditions.map((condition) => ({
+    id: condition.id,
+    name: condition.name,
+    icon: condition.texture,
+    // Compatibiliy shims with Foundry
+    get label() {
+      return this.name;
+    },
+    set label(name) {
+      this.name = name;
+    },
+  }));
   if (game.settings.get("pf1", "coreEffects")) sys.push(...core);
 
   sys.sort((a, b) => a.label.localeCompare(b.label));

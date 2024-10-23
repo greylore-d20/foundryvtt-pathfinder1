047c0d6c464ce50f0be582ac95fc5da1a7463e67
KitCat <katsulynx@gmail.com>
2024-10-23 20:05:24 +0100

    Snap ruler text and outline to highlighted squares for cone templates

--- module/canvas/measure.mjs
+++ module/canvas/measure.mjs
@@ -1,5 +1,3 @@
-import { measureDistance } from "@utils";
-
 const withinAngle = (min, max, value) => {
   min = Math.normalizeDegrees(min);
   max = Math.normalizeDegrees(max);
@@ -74,6 +72,78 @@ export class TemplateLayerPF extends TemplateLayer {
 
 export class MeasuredTemplatePF extends MeasuredTemplate {
   /**
+   * Calculates a set of x & y coordinates that the template actually should have based on type and origin
+   *
+   * @returns {{x: number, y: number}}
+   * @private
+   */
+  _getTemplateSnapCoordinates() {
+    let { x, y } = this.document;
+    const grid = canvas.grid;
+
+    if (this.document.t === "cone" && game.canvas.grid.isSquare) {
+      const angle = this.document.direction;
+
+      if (angle <= 45 || angle >= 315) {
+        x = Math.ceil(x / grid.size) * grid.size;
+      } else if (angle >= 135 && angle <= 225) {
+        x = Math.floor(x / grid.size) * grid.size;
+      }
+
+      if (angle >= 45 && angle <= 135) {
+        y = Math.ceil(y / grid.size) * grid.size;
+      } else if (angle >= 225 && angle <= 315) {
+        y = Math.floor(y / grid.size) * grid.size;
+      }
+    }
+
+    return { x, y };
+  }
+
+  /**
+   * Recalculate template visual element positions based on snap coordinates
+   *
+   * @private
+   */
+  _setElementOffsets() {
+    const { x: snapX, y: snapY } = this._getTemplateSnapCoordinates();
+    const offsetX = snapX - this.document.x;
+    const offsetY = snapY - this.document.y;
+
+    this.template.x = offsetX;
+    this.template.y = offsetY;
+
+    this.ruler.position.set(this.ray?.dx + 10 + offsetX, this.ray?.dy + 5 + offsetY);
+  }
+
+  /**
+   * @override
+   * @private
+   */
+  _refreshRulerText() {
+    super._refreshRulerText();
+    this._setElementOffsets();
+  }
+
+  /**
+   * @override
+   * @private
+   */
+  _refreshPosition() {
+    super._refreshPosition();
+    this._setElementOffsets();
+  }
+
+  /**
+   * @override
+   * @private
+   */
+  _refreshTemplate() {
+    super._refreshTemplate();
+    this._setElementOffsets();
+  }
+
+  /**
    * Get highlighted square coordinates.
    *
    * Supports only circle, cone and ray templates.
@@ -94,24 +164,7 @@ export class MeasuredTemplatePF extends MeasuredTemplate {
     }
 
     const grid = canvas.grid;
-    let { x: ox, y: oy } = this.document;
-
-    // Snap cones to the closest corner or vertex based on their direction
-    if (templateType === "cone") {
-      const angle = this.document.direction;
-
-      if (angle <= 45 || angle >= 315) {
-        ox = Math.ceil(ox / grid.size) * grid.size;
-      } else if (angle >= 135 && angle <= 225) {
-        ox = Math.floor(ox / grid.size) * grid.size;
-      }
-
-      if (angle >= 45 && angle <= 135) {
-        oy = Math.ceil(oy / grid.size) * grid.size;
-      } else if (angle >= 225 && angle <= 315) {
-        oy = Math.floor(oy / grid.size) * grid.size;
-      }
-    }
+    const { x: ox, y: oy } = this._getTemplateSnapCoordinates();
 
     // Test if template origin is in the center of a grid space. Apply a grace margin for odd grid sizes
     const originInCenter = ox % grid.size === Math.ceil(grid.size / 2) && oy % grid.size === Math.ceil(grid.size / 2);

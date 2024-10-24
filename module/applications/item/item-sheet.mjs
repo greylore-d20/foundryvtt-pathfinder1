bf6ce19716529494002d70236d106c682a272381
KitCat <katsulynx@gmail.com>
2024-10-24 17:23:29 +0000

    fix: prevent change tooltip from recreating if current instance still exists
    Fixes #3722

--- module/applications/item/item-sheet.mjs
+++ module/applications/item/item-sheet.mjs
@@ -2109,12 +2109,14 @@ export class ItemSheetPF extends ItemSheet {
     content.querySelector(".delete").addEventListener("click", (ev) => this._onDeleteChange(ev, el));
     content.querySelector(".edit").addEventListener("click", (ev) => this._onEditChange(ev, el, true));
 
-    await game.tooltip.activate(el, {
-      content,
-      locked: true,
-      direction: TooltipManager.TOOLTIP_DIRECTIONS.LEFT,
-      cssClass: "pf1 change-menu",
-    });
+    if (!document.querySelector(`.locked-tooltip.pf1.change-${changeId}`)) {
+      await game.tooltip.activate(el, {
+        content,
+        locked: true,
+        direction: TooltipManager.TOOLTIP_DIRECTIONS.LEFT,
+        cssClass: "pf1 change-menu change-" + changeId,
+      });
+    }
   }
 
   /**
